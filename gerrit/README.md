53e6783b60292e0797c46fad66c957c6260fd8aa
Matt Caddell <mjcaddell1@mac.com>
2024-02-07 10:38:35 -0600

    docs: add retirement announcement

--- gerrit/README.md
+++
@@ -1,5 +0,0 @@
-# DEPRECATED
-
-Code moved to  [trueaccord/docker-gerrit](https://github.com/trueaccord/docker-gerrit)
-
-Image moved to [quay.io/trueaccord/gerrit](https://quay.io/trueaccord/gerrit)
--- gerrit/init_or_upgrade.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-echo Installing plugins
-java -jar $GERRIT_WAR init --batch -d /gerrit --no-auto-start --install-plugin=replication --install-plugin=reviewnotes --install-plugin=download-commands
-
-echo Running step 2...
-# Rerun the script to download MySQL-connector, bouncy castle, etc.
-java -jar $GERRIT_WAR init --batch -d /gerrit --no-auto-start
-
